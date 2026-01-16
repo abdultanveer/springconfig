8a05497b80eaa39ff250f6d5ae4d05a648bf2e79
abdul <abdul.tanveer@gmail.com>
2026-01-16 11:48:32 +0530

    lists and maps in app.properties

--- src/main/java/com/abdul/config/demo/MyController.java
+++ src/main/java/com/abdul/config/demo/MyController.java
@@ -4,14 +4,23 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
 public class MyController {
     @Value("${my.greeting:default-value}")
     String myMessage;
 
+    @Value("${my.list.values}")
+    List<String> values;
+
+    @Value("#{${db.values}}")
+    Map<String,String> dbValues;
+
     @GetMapping("/greeting")
     public  String greeting(){
-        return  myMessage;
+        return  myMessage+values+dbValues;
     }
 
 
