e1ae1118af6e841893f8652a27acc9afbfd4354f
abdul <abdul.tanveer@gmail.com>
2026-01-16 12:59:54 +0530

    selecting beans based on profiles

---
+++ src/main/java/com/abdul/config/demo/DataSourceBean.java
@@ -0,0 +1,11 @@
+package com.abdul.config.demo;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
+
+@Repository
+//@Profile("test")
+//@Profile("default") --compiler added
+
+public class DataSourceBean {
+}
